99f40d287f8b785d00d2906c5fa0f4654d95d08f
adm_sterling <adamsterling10@gmail.com>
2020-12-18 13:58:55 +0000

    start vue

--- vue/src/main.js
+++ vue/src/main.js
@@ -1,27 +1,12 @@
 import Vue from 'vue';
 import App from './App.vue';
 import vuetify from './plugins/vuetify';
+import store from './store';
 import router from './router/router';
-import VueApollo from 'vue-apollo';
-
-import { ApolloClient } from 'apollo-client';
-import { createHttpLink } from 'apollo-link-http';
-import { InMemoryCache } from 'apollo-cache-inmemory';
-
-Vue.use(VueApollo);
-const { api_port } = require('../../config');
-
-const apolloClient = new ApolloClient({
-  link: createHttpLink({ uri: 'http://localhost:' + api_port + '/graphql' }),
-  cache: new InMemoryCache(),
-});
-
-const apolloProvider = new VueApollo({
-  defaultClient: apolloClient,
-});
 
 new Vue({
   vuetify,
+  store,
   router,
   apolloProvider,
   render: (h) => h(App),
