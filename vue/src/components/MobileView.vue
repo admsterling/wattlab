9bbba818c665e95b0de552dcd85aa818fd0ce481
adm_sterling <adamsterling10@gmail.com>
2021-02-24 17:21:35 +0000

    MobileView

--- vue/src/components/MobileView.vue
+++ vue/src/components/MobileView.vue
@@ -1,9 +1,25 @@
 <template>
   <div>
     <v-card class="ma-3">
-      <v-card-title> Sorry... </v-card-title>
-
-      <v-card-text>Mobile support is not available.</v-card-text>
+      <v-card-title class="text-h2"> Sorry...</v-card-title>
+      <v-divider></v-divider>
+      <v-card-text>
+        <v-container class="text-center text-subtitle-1">
+          <v-row>
+            <v-col cols="12">Mobile Device support is not available.</v-col>
+          </v-row>
+          <v-row>
+            <v-col cols="12">
+              Please view this site on a laptop or monitor.
+            </v-col>
+          </v-row>
+          <v-row>
+            <v-col cols="12">
+              <v-icon class="ml-2">mdi-emoticon-sad-outline</v-icon>
+            </v-col>
+          </v-row>
+        </v-container>
+      </v-card-text>
     </v-card>
   </div>
 </template>
